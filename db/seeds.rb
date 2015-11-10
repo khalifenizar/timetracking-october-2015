6420e9d7328daf2668640f899ded41901af704eb
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-11-10 10:52:37 -0500

    Add seeds file with duplication prevention

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Don't use .create since we are not sure if we want to save yet
+new_projects = [
+  Project.new(name: "Ironhack", description: "The best coding school."),
+  Project.new(name: "Wendy's", description: "Okay burgers. Extremely unhealthy."),
+  Project.new(name: "Toasted", description: "Website for toasted."),
+  Project.new(name: "Building.co", description: "Events with Building.co."),
+]
+
+new_projects.each do |proj|
+  # Check if project already exists
+  if Project.find_by(name: proj.name) == nil
+    # If it doesn't exist (nil), save it
+    puts "Creating Project: #{proj.name}"
+    proj.save
+  end
+end
+
+puts "Seeds end!"
