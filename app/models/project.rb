b457f7d4e66f99ffa899a8dd134ed2e9c5e8d236
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-11-11 16:05:08 -0500

    Add validations to Entry and Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :entries
+
+  validates(:name, { uniqueness: true })
+  validates(:name, { presence: true })
+  validates(:name, { length: { maximum: 30 } })
 end
